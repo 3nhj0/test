af3c3d537992b8af27f5044d2f5c52a1214ae222
3nhj0 <97040301+3nhj0@users.noreply.github.com>
2023-09-30 18:38:40 +0800

    Update and rename alert1.html to alert1.js

---
+++ alert1.js
@@ -0,0 +1,16 @@
+// This is a JavaScript comment. It won't be executed by the computer.
+
+// Define a function called greetUser
+function greetUser(name) {
+  console.log(`Hello, ${name}!`);
+}
+
+// Call the greetUser function with the argument "John"
+greetUser("John");
+
+// Call the greetUser function with the argument "Jane"
+greetUser("Jane");
+
+document.write(`
+<h1>Test</h1>
+`);

